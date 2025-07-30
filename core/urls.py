23f3b7b2035f28e9c5a60d7e7eb4b8badf86882b
Mark-AImagineers <148505322+Mark-AImagineers@users.noreply.github.com>
2025-07-30 15:12:35 +0800

    public /landing

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import render
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", lambda request: render(request, "landing.html"), name="landing"),
     path("", include("users.urls")),
 ]
