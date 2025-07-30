5b38ecf88ea12e0c967d9ed1dd96c53d366f928c
Mark-AImagineers <148505322+Mark-AImagineers@users.noreply.github.com>
2025-07-30 17:17:55 +0800

    added subscription middleware - auto expire

---
+++ users/middleware.py
@@ -0,0 +1,19 @@
+from django.utils import timezone
+
+class SubscriptionMiddleware:
+    """
+    Downgrade user to Free if their subscription_expires is past.
+    """
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        user = getattr(request, "user", None)
+        if user and user.is_authenticated:
+            expires = getattr(user, "subscription_expries", None)
+            if expires and timezone.now() > expires:
+                user.subscription_level = user.SUBSCRIPTION_FREE
+                user.subscription_expires = None
+                user.save()
+
+        return self.get_response(request)
