9d4603b67a024d236a52a2231739bb7d46f2f885
Mark-AImagineers <148505322+Mark-AImagineers@users.noreply.github.com>
2025-07-30 17:04:42 +0800

    added subscription and expiration

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from users.models import EmailUser
 
-# Register your models here.
+@admin.register(EmailUser)
+class EmailUserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'subscription_level', 'subscription_expires', 'is_active')
--- users/models.py
+++ users/models.py
@@ -17,12 +17,38 @@ class UserManager(BaseUserManager):
         extra_fields.setdefault("is_superuser", True)
         return self.create_user(email, password, **extra_fields)
     
+SUBSCRIPTION_FREE = 0
+SUBSCRIPTION_BASIC = 1
+SUBSCRIPTION_PRO = 2
+SUBSCRIPTION_ENTERPRISE = 3
+
+SUBSCRIPTION_CHOICES = [
+    (SUBSCRIPTION_FREE, "Free"),
+    (SUBSCRIPTION_BASIC, "Basic"),
+    (SUBSCRIPTION_PRO, "Pro"),
+    (SUBSCRIPTION_ENTERPRISE, "Enterprise"),
+]
 
 class EmailUser(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     date_joined = models.DateTimeField(default=timezone.now)
+
+    subscription_expires = models.DateTimeField(null=True, blank=True, help_text="When the current subscription level expries. Auto-resets to Free")
+
+    subscription_level = models.PositiveSmallIntegerField(
+        choices=SUBSCRIPTION_CHOICES,
+        default=SUBSCRIPTION_FREE,
+        help_text="0=Free, 1=Basic, 2=Pro, 3=Enterprise"
+    )
+
+    def save(self, *args, **kwargs):
+        if self.subscription_expires and timezone.now() > self.subscription_expries:
+            self.subscription_level = SUBSCRIPTION_FREE
+            self.subscription_expires = None
+            super().save(*args, **kwargs)
+    
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
